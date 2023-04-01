2fd91c2e3dd146ba69de7ceff3afda6a885e1e56
alcnaka <47954083+alcnaka@users.noreply.github.com>
2023-04-02 05:42:04 +0900

    error handler

--- simple_discord_tts/cogs/read_queue_task.py
+++ simple_discord_tts/cogs/read_queue_task.py
@@ -26,6 +26,7 @@ class ReadQueueCog(commands.Cog):
             voice_client = await ctx.voice_channel.connect()
         except ClientException as e:
             logger.info('already joined')
+            logger.info(f'VoiceClientCount: {len(self.bot.voice_clients)}')
             voice_client = self.bot.voice_clients[0]
             if type(voice_client) != VoiceClient:
                 logger.warning(voice_client)
@@ -36,16 +37,19 @@ class ReadQueueCog(commands.Cog):
         if type(voice_client) != VoiceClient:
             return
 
-        with NamedTemporaryFile() as f:
-            wav = (await tts(ctx.text))
-            f.write(wav)
-            audio_source = FFmpegPCMAudio(f.name)
-            audio_source = PCMVolumeTransformer(audio_source, 0.18)
+        try:
+            with NamedTemporaryFile() as f:
+                wav = (await tts(ctx.text))
+                f.write(wav)
+                audio_source = FFmpegPCMAudio(f.name)
+                audio_source = PCMVolumeTransformer(audio_source, 0.18)
+                with wave.open(f.name) as wr:
+                    pt = wr.getnframes() / wr.getframerate()
 
-            voice_client.play(audio_source)
-            with wave.open(f.name) as wr:
-                pt = wr.getnframes() / wr.getframerate()
-            await asyncio.sleep(pt + 0.2)
+                voice_client.play(audio_source)
+                await asyncio.sleep(pt + 0.2)
+        except Exception:
+            logger.exception('some errors in read_queue')
 
 
     @tasks.loop(seconds=1)
