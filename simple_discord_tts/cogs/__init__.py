3598b259190ee4efed4e770633d2f299ad422a5c
alcnaka <47954083+alcnaka@users.noreply.github.com>
2023-08-04 23:58:20 +0900

    VC Stateの読み上げを追加

--- simple_discord_tts/cogs/__init__.py
+++ simple_discord_tts/cogs/__init__.py
@@ -1,9 +1,11 @@
 from .read_queue_task import ReadQueueCog
 from .clear_command import ClearCog
 from .vc_command import ControlCommand
+from .voice_state_listener import VoiceStateListenerCog
 
 all_cogs = [
     ReadQueueCog,
     ClearCog,
     ControlCommand,
+    VoiceStateListenerCog,
 ]
---
+++ simple_discord_tts/cogs/voice_state_listener.py
@@ -0,0 +1,62 @@
+from logging import getLogger
+
+from discord import VoiceState, Member, VoiceChannel
+from discord.ext import commands
+from simple_discord_tts.app import TTSBot
+
+from ..app import TTSContext
+
+
+logger = getLogger(__name__)
+
+
+class VoiceStateListenerCog(commands.Cog):
+    def __init__(self, bot: TTSBot) -> None:
+        logger.debug('VoiceStateListenCog is initialized')
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self, member: Member, before: VoiceState, after: VoiceState
+    ) -> None:
+        # 同一チャンネル内の動作は無視
+        if before.channel == after.channel:
+            return
+
+        callme = member.display_name
+
+        if after.channel is not None:
+            bot_vc = after.channel.guild.voice_client
+            if (
+                bot_vc
+                and after.channel == bot_vc.channel
+                and type(after.channel) == VoiceChannel
+            ):
+                t = f"{callme} が参加しました。"
+                logger.debug(t)
+                tts_ctx = TTSContext(voice_channel=after.channel, text=t)
+                await self.bot.queue.put(tts_ctx)
+
+        if after.channel is None and before.channel is not None:
+            bot_vc = before.channel.guild.voice_client
+            if (
+                bot_vc
+                and before.channel == bot_vc.channel
+                and type(before.channel) == VoiceChannel
+            ):
+                t = f"{callme} が切断しました。"
+                logger.debug(t)
+                tts_ctx = TTSContext(voice_channel=before.channel, text=t)
+                await self.bot.queue.put(tts_ctx)
+
+        if before.channel is not None and after.channel is not None:
+            bot_vc = before.channel.guild.voice_client
+            if (
+                bot_vc
+                and before.channel == bot_vc.channel
+                and type(before.channel) == VoiceChannel
+            ):
+                t = f"{callme} が移動しました。"
+                logger.debug(t)
+                tts_ctx = TTSContext(voice_channel=before.channel, text=t)
+                await self.bot.queue.put(tts_ctx)
