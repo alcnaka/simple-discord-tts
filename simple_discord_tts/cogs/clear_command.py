db36149733b0f43e5cdaee1157654225bb3dbeed
alcnaka <47954083+alcnaka@users.noreply.github.com>
2023-09-12 10:56:56 +0900

    update clear logic
    remove duplicated logics.

--- simple_discord_tts/cogs/clear_command.py
+++ simple_discord_tts/cogs/clear_command.py
@@ -3,6 +3,7 @@ from logging import getLogger
 from typing import Self
 
 from discord import TextChannel
+from discord.errors import Forbidden
 from discord.ext import commands, tasks
 from discord.ext.commands import Context
 from zoneinfo import ZoneInfo
@@ -26,16 +27,22 @@ class ClearCog(commands.Cog):
     async def auto_clear(self: Self) -> None:
         await self._clear()
 
-    async def _clear(self: Self) -> None:
+    async def _clear(self: Self, before_minutes: float = 10) -> None:
         """10分以上前のテキストを削除."""
-        logger.debug("clear start")
+        logger.debug("clear start(offset: %s minutes)", before_minutes)
         channel = self.bot.get_channel(self.bot.listen_channel_id)
-        before = datetime.now(JST) - timedelta(minutes=10)
+        before = datetime.now(JST) - timedelta(minutes=before_minutes)
+        two_weeks_ago = datetime.now(JST) - timedelta(days=14)
         if type(channel) == TextChannel:
-            msgs = [m async for m in channel.history(before=before)]
+            msgs = [
+                m async for m in channel.history(before=before, after=two_weeks_ago)
+            ]
             if len(msgs):
-                logger.debug(f"try to delete messages: {len(msgs)}")
-            await channel.delete_messages(msgs)
+                logger.debug("try to delete messages: %s", len(msgs))
+            try:
+                await channel.delete_messages(msgs)
+            except Forbidden:
+                logger.exception("Don't have permission")
         logger.debug("clear end")
 
     @commands.command()
@@ -48,14 +55,5 @@ class ClearCog(commands.Cog):
     async def clear_all(self: Self, _: Context) -> None:
         """全テキストを削除."""
         logger.debug("command: clear_all start")
-        channel = self.bot.get_channel(self.bot.listen_channel_id)
-        two_weeks_ago = datetime.now(JST) - timedelta(days=14)
-        if type(channel) == TextChannel:
-            try:
-                msgs = [m async for m in channel.history(after=two_weeks_ago)]
-                logger.debug(f"try to delete messages: {len(msgs)}")
-                await channel.delete_messages(msgs)
-            except Exception:
-                logger.exception("メッセージの削除中に例外が発生しました。")
-            # TODO: レートリミットに引っかかる
+        await self._clear(before_minutes=0)
         logger.debug("command: clear_all end")
